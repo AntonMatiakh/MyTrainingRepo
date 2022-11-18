faca1117d33501ac8b192fef89c6eceec815890b
Anton_Matiakh <ron1katc88@gmail.com>
2022-11-18 11:36:53 +0200

    homework #6 variant #1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import com.academy.models.Course;
 import com.academy.models.Lecture;
 
 public class Main {
     public static void main(String[] args) {
 
-        Lecture firstLecture = new Lecture();
-        Lecture secondLecture = new Lecture();
-        Lecture thirdLecture = new Lecture();
-        Lecture fourthLecture = new Lecture();
-        Lecture fifthLecture = new Lecture();
+        Course firstCourse = new Course(777l);
 
-        System.out.println(Lecture.counter);
+        Lecture firstLecture = new Lecture();
+        firstLecture.courseID = firstCourse.getID();
+        Lecture secondLecture = new Lecture();
+        secondLecture.courseID = firstCourse.getID();
+        Lecture thirdLecture = new Lecture();
+        thirdLecture.courseID = firstCourse.getID();
+        Lecture fourthLecture = new Lecture();
+        fourthLecture.courseID = firstCourse.getID();
+        Lecture fifthLecture = new Lecture();
+        fifthLecture.courseID = firstCourse.getID();
+        Lecture sixthLecture = new Lecture();
+        sixthLecture.courseID = firstCourse.getID();
+
+        System.out.println("Number of lectures: " + Lecture.counter);
+        System.out.println("Sixth lecture's id: " + sixthLecture.courseID);
 
     }
 }
--- src/com/academy/models/Lecture.java
+++ src/com/academy/models/Lecture.java
@@ -3,6 +3,7 @@ package com.academy.models;
 public class Lecture {
     private long ID;
     public static long counter;
+    public long courseID;
 
     private Homework homework;
     private ExtraMaterial extraMaterial;
