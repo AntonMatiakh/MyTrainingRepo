9d6166bcea47ca3f6ee84f65d96937d90c1dbb3b
Anton_Matiakh <ron1katc88@gmail.com>
2022-11-25 10:36:47 +0200

    all changes saved

--- src/com/academy/services/LectureService.java
+++ src/com/academy/services/LectureService.java
@@ -4,11 +4,6 @@ import com.academy.models.Lecture;
 
 public class LectureService {
 
-//    private long ID;
-//    public static long counter;
-//    public long courseID;
-//    private String name;
-
     public Lecture createLecture(String name, long ID, long courseID){
         return new Lecture(name, ID, courseID);
     }
